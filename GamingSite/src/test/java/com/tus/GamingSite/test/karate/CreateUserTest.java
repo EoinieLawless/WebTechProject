406d33bfb871b5e4531c885a2785eda337b6b7be
Eoin Lawless <Eoinie.lawless@gmail.com>
2025-03-31 13:19:13 +0100

    test Jenkins trigger

--- GamingSite/src/test/java/com/tus/GamingSite/test/karate/CreateUserTest.java
+++ GamingSite/src/test/java/com/tus/GamingSite/test/karate/CreateUserTest.java
@@ -1,11 +1,14 @@
-//package com.tus.GamingSite.test.karate;
-//
-//import com.intuit.karate.junit5.Karate;
-//
-//class CreateUserTest {
-//    
-//    @Karate.Test
-//    Karate testCreateUser() {
-//        return Karate.run("classpath:features/create-user.feature");
-//    }
-//}
+package com.tus.GamingSite.test.karate;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.intuit.karate.junit5.Karate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class CreateUserTest {
+    
+    @Karate.Test
+    Karate testCreateUser() {
+        return Karate.run("classpath:features/create-user.feature");
+    }
+}
